9c23dfe126ba470a6545b8e9794eafc30bf50f40
hermawanmangambali <hermawanalimangambali@gmail.com>
2023-11-14 03:08:58 +0700

    upload kode

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html, js}"],
+  theme: {
+    extend: {
+      width: {
+        '81': '22rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
